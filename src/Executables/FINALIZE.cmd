a6fadfc0de2233ff83cf5705f71790c9b6f4a5d2
Bae Joon Hoo <joonhoo0524@naver.com>
2023-09-23 10:47:05 +0000

    d

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -45,6 +45,8 @@ PowerShell -NonInteractive -NoLogo -NoP -C "& {$tmp = (New-TemporaryFile).FullNa
 del /q /f "%SYSTEMDRIVE%\Windows\System32\drivers\etc\hosts"
 copy /y "hosts" "%SYSTEMDRIVE%\Windows\System32\drivers\etc\hosts"
 
+start "" "C:\Windows\explorer.exe"
+
 start "" "C:\Windows\Resources\Themes\dark.theme" & timeout /t 3 & taskkill /im "systemsettings.exe" /f
 
 @REM echo Configuring UAC...
