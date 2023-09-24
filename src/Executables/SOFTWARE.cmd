a3e51348f6b84192dff91d47b70908289a3961bd
Bae Joon Hoo <joonhoo0524@naver.com>
2023-09-24 04:09:34 +0000

    Fix not installing software correctly

--- src/Executables/SOFTWARE.cmd
+++ src/Executables/SOFTWARE.cmd
@@ -1,19 +1,21 @@
 @echo off
 
 @REM Steam
-wget https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe -o"SteamSetup.exe"
+wget "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
 start /wait SteamSetup.exe /S
 
 @REM Visual Studio Code
-wget https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user -o"vsc.exe"
-start /wait vsc.exe /LOADINF="vsc.inf" /VERYSILENT
+wget "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -O "VisualStudioCode.exe"
+start /wait VisualStudioCode.exe /LOADINF="vsc.inf" /VERYSILENT
 
 @REM Jetbrains
 choco install jetbrainstoolbox
 choco install phpstorm
 
 @REM Discord Canary + Vencord
-wget https://discordapp.com/api/download/canary?platform=win -o"DiscordCanary.exe"
+wget "https://discordapp.com/api/download/canary?platform=win" -O "DiscordCanary.exe"
 start /wait DiscordCanary.exe --silent
-wget https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe -o"vencord.exe"
-start /wait vencord.exe -branch canary -install
+timeout 15
+
+wget "https://github.com/Vencord/Installer/releases/latest/download/VencordInstallerCli.exe" -O "VencordInstallerCli.exe"
+start /wait VencordInstallerCli.exe -branch canary -install
